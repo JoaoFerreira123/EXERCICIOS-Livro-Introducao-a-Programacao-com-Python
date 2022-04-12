24b2448001bf1d199ff78826c09c6aa75411e1a6
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-04-12 09:30:09 -0300

    ok4

--- Cap. 4/Ex4.3V1.py
+++ Cap. 4/Ex4.3V1.py
@@ -7,5 +7,16 @@ n3 = int(input('Digite o 3º Número: '))
 maior = n1
 menor = n1
 
-if n2 > maior 
+if n2 > n1 and n2 > n3:
+    maior = n2
+elif n3 > n1 and n3 > n2:
+    maior = n3
+
+if n2 < n1 and n2 < n3:
+    menor = n2
+elif n3 < n1 and n3 < n2:
+    menor = n3
+
+print(f'O maior número é {maior}')
+print(f'O menor número é o {menor}')
 
