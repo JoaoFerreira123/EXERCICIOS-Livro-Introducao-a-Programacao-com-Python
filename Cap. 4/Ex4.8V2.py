39681814e7e1d3d113cd4a11e8659d8d61f88738
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-04-13 09:26:26 -0300

    ok

---
+++ Cap. 4/Ex4.8V2.py
@@ -0,0 +1,22 @@
+# Escreva um programa que leia dois números e que pergunte qual operação você deseja realizar. 
+# Você deve poder calcular soma (+), subtração (-), multiplicação (*) e divisão (/). 
+# Exiba o resultado da operação solicitada
+
+print('-'*20)
+n1 = int(input('Digite o 1º número: '))
+n2 = int(input('Digite o 2º número: '))
+print('-'*20)
+print('+ -> soma\n- -> Subtração\n* -> Multiplicação\n/ -> Divisão')
+op = input('Insira qual operação você quer realizar: ')
+
+#estrutura switch case, implementada no Python 3.9
+match op:
+    case '+':
+        print('n1+n2')
+    case '-':
+        print('n1-n2')
+    case '*':
+        print(n1*n2)
+    case '/':
+        print('n1/n2')
+    
