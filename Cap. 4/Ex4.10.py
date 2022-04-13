fb4c98f1d9c32f76f30072b4f94524870cd7548b
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-04-13 15:49:29 -0300

    ok

--- Cap. 4/Ex4.10.py
+++
@@ -1,9 +0,0 @@
-# Escreva um programa que calcule o preço a pagar pelo fornecimento de energia elétrica. 
-# Pergunte a quantidade de kWh consumida e o tipo de instalação: 
-# R para residências, I para indústrias e C para comércios. 
-# Calcule o preço a pagar de acordo com a tabela a seguir. (tabela pg 82)
-
-qtde = float(input('Quantos KWh foram consumidos? '))
-print('\nR para RESIDÊNCIAS\nI para INDÚSTRIAS\nC para COMÉRCIOS\n')
-tipo = input('Qual o tipo de instalação? ')
-
