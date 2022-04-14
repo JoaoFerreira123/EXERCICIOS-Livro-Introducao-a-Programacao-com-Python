2a65804bc69ad1a5c55a4b8821685b76d2f55797
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-04-13 21:07:39 -0300

    ok

--- Cap. 4/Ex4.10V2.py
+++ Cap. 4/Ex4.10V2.py
@@ -6,23 +6,25 @@
 
 qtde = float(input('Quantos KWh foram consumidos? '))
 print('\nR para RESIDÊNCIAS\nI para INDÚSTRIAS\nC para COMÉRCIOS\n')
-tipo = input('Qual o tipo de instalação? ')
-v = 0
+tipo = input('Qual o tipo de instalação? ').upper()
+
 match tipo:
-    case 'Rr':
+    case 'R':
         if qtde > 500:
             v = qtde * 0.65
         else:
             v = qtde * 0.4
-    case 'Cc':
+    case 'C':
         if qtde > 1000:
             v = qtde * 0.6
         else:
             v = qtde * 0.55
-    case 'Ii':
+    case 'I':
         if qtde > 5000:
             v = qtde * 0.6
         else:
             v = qtde * 0.55
+    case _:
+        print('Valor inválido')
 
-print(f'O valor a ser pago é de R$: {v}')
+print(f'O valor a ser pago é de R$: {v:.2f}')
