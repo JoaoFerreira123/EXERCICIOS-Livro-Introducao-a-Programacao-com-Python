42d909a3e2830ccf7a84b1fb9747711548555479
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-04-20 16:59:16 -0300

    ok

--- Cap. 5/Ex5.22.py
+++ Cap. 5/Ex5.22.py
@@ -1,3 +1,24 @@
 # Escreva um programa que exiba uma lista de opções (menu): adição, subtração, divisão, multiplicação e sair. 
 # Imprima a tabuada da operação escolhida. Repita até que a opção saída seja escolhida
 
+
+print('+ -> soma\n- -> subtracao\n* -> multiplicacao\n/ -> divisao\n0 -> sair')
+while True:
+    x = 0
+    op = input('Insira uma opção: ')
+    num = int(input('Insira o número: '))
+    if op == '0':
+        break
+    elif op in '+-*/':
+        while x <=10:
+            if op ==  '+':
+                print(f'{num} + {x} = {num+x}')
+            if op == '-':
+                print(f'{num} - {x} = {num-x}')
+            if op == '*':
+                print(f'{num} x {x} = {num*x}')
+            if op == '/':
+                print(f'{num} / {x} = {num/x}')
+            x += 1
+    else:
+        print('Opção Inválida')
