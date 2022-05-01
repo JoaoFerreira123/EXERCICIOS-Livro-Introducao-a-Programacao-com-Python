78923c57974f2630326d331e05557eef5b5c30e3
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-05-01 10:32:02 -0300

    ok

--- Cap. 9/teste02.py
+++ Cap. 9/teste02.py
@@ -1,5 +1,5 @@
-arquivo = open('teste01.txt', 'r')
-
+arquivo = open('C:/Users/joaoa/Desktop/Programação/Python/EXERCÍCIOS - Livro Introdução a Programação com Python/Cap. 9/teste01.txt', 'r')
+#só funcionou colocando todo o path, mesmo estando na mesma pasta
 for i in arquivo.readlines():
     print(i)
 
