b73dd101d765d99fda3fe7f0338f3d6053dda25c
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-05-02 17:41:06 -0300

    ok

---
+++ Cap. 9/Exemplo9.06.py
@@ -0,0 +1,115 @@
+# PrograMa 9.6 - Controle de uMa agenda de telefones
+
+from curses.ascii import alt
+
+
+agenda = []
+
+def pede_nome():
+    return input('Nome: ')
+
+def pede_telefone():
+    return input('Telefone: ')
+
+def mostra_dados(nome, telefone):
+    print(f'Nome: {nome} - Telefone: {telefone}')
+
+def pede_nome_arquivo():
+    return input('Nome do arquivo: ')
+
+def pesquisa(nome):
+    mnome = nome.lower()
+    for p, e in enumerate(agenda):
+        if e[0].lower() == mnome:
+            return p
+    return None
+
+def novo():
+    global agenda
+    nome = pede_nome()
+    telefone = pede_telefone()
+    agenda.append([nome, telefone])
+
+def apaga():
+    global agenda
+    nome = pede_nome()
+    p = pesquisa(nome)
+    if p is not None:
+        del agenda[p]
+    else:
+        print('Nome não encontrado')
+
+def altera():
+    p = pesquisa(pede_nome())
+    if p is not None:
+        nome = agenda[p][0]
+        telefone = agenda[p][1]
+        print('Encontrado: ')
+        mostra_dados(nome, telefone)
+        nome = pede_nome()
+        telefone = pede_telefone()
+        agenda[p] = [nome, telefone]
+    else:
+        print('Nome não encontrado')
+
+def lista():
+    print('\nAgenda\n\n--------')
+    for e in agenda:
+        mostra_dados(e[0], e[1])
+    print('-------------')
+
+def lê(): #le arquivo e grava na agenda
+    global agenda
+    nome_arquivo = pede_nome_arquivo()
+    with open(nome_arquivo, 'r', encoding = 'utf-8') as arquivo:
+        agenda = []
+        for l in arquivo.readlines():
+            nome, telefone = l.strip().split('#') #cria uma lista com nome e tel 
+            agenda.append([nome, telefone])
+
+def grava(): #le agenda e grava no arquivo
+    nome_arquivo = pede_nome_arquivo()
+    with open(nome_arquivo, 'w', encoding='utf-8') as arquivo:
+        for e in agenda:
+            arquivo.write(f'{e[0]}#{e[1]}\n')
+
+def valida_faixa_inteiro(pergunta, inicio, fim):
+    while True:
+        try:
+            valor = int(input(pergunta))
+            if inicio <= valor <= fim:
+                return valor
+        except ValueError:
+            print(f'Valor Inválido, favor digitar entre {inicio} e {fim}')
+
+def menu():
+    print("""
+    1 - Novo
+    2 - Altera
+    3 - Apaga
+    4 - Lista
+    5 - Grava
+    6 - Lê
+
+    0 - Sai
+    """)
+    return valida_faixa_inteiro('Escolha uma opção: ', 0, 6)
+
+while True:
+    opcao = menu()
+    match (opcao):
+        case 0:
+            break
+        case 1:
+            novo()
+        case 2:
+            altera()
+        case 3:
+            apaga()
+        case 4:
+            lista()
+        case 5:
+            grava()
+        case 6:
+            lê()
+            
