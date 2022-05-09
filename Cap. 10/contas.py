0a4bfb241a90ed6002e23e56361dd4febb7c02cf
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-05-08 23:08:08 -0300

    ok

--- Cap. 10/contas.py
+++ Cap. 10/contas.py
@@ -3,6 +3,8 @@ class Conta:
         self.saldo = saldo
         self.clientes = clientes
         self.numero = numero
+        self.operacoes = []
+        self.deposito(saldo)
 
     def resumo(self):
         print(f'CC Número: {self.numero} - Saldo: {self.saldo:10.2f}')
@@ -10,7 +12,14 @@ class Conta:
     def saque(self, valor):
         if self.saldo >= valor:
             self.saldo -= valor
+            self.operacoes.append(['SAQUE', valor])
     
     def deposito(self, valor):
         self.saldo += valor
-        
+        self.operacoes.append(['DEPÓSITO', valor])
+
+    def extrato(self):
+        print(f'Extrato CC Nº {self.numero}\n')
+        for i in self.operacoes:
+            print(f'{i[0]:10s} {i[1]:10.2f}')
+        print(f'\n  Saldo: {self.saldo:10.2f}\n')
--- Cap. 10/teste.py
+++ Cap. 10/teste.py
@@ -6,3 +6,10 @@ maria = Cliente('Maria da Silva', '789-456')
 
 conta1 = Conta(joao, 236, 50)
 conta2 = Conta(maria, 542)
+
+conta1.saque(50)
+conta2.deposito(300)
+conta1.deposito(230)
+conta2.saque(75)
+conta1.extrato()
+conta2.extrato()
